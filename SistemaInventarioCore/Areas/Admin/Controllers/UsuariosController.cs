73086cf710ee7f7f3ba4a98b536f672768c9ad91
ingcarlosayala <ing.carlos.ayala12@gmail.com>
2023-07-05 09:59:11 -0400

    Administracion de Usuarios

---
+++ SistemaInventarioCore/Areas/Admin/Controllers/UsuariosController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaInventarioCore.AccesoDato.Data;
+using SistemaInventarioCore.AccesoDato.Repositorio.IRepositorio;
+
+namespace SistemaInventarioCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsuariosController : Controller
+    {
+        private readonly IUnidadTrabajo unidadTrabajo;
+        private readonly ApplicationDbContext dbContext;
+
+        public UsuariosController(IUnidadTrabajo unidadTrabajo,ApplicationDbContext dbContext)
+        {
+            this.unidadTrabajo = unidadTrabajo;
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API
+
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var usuarioLista = await unidadTrabajo.UsuarioAplicacion.ObtenerTodos();
+            var userRole = await dbContext.UserRoles.ToListAsync();
+            var roles = await dbContext.Roles.ToListAsync();
+
+            foreach (var usuario in usuarioLista)
+            {
+                var roleId = userRole.FirstOrDefault(u => u.UserId == usuario.Id).RoleId;
+                usuario.Role = roles.FirstOrDefault(u => u.Id == roleId).Name;
+            }
+
+            return Json(new { data = usuarioLista });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BloquearDesbloquear([FromBody] string id)
+        {
+            var usuario = await unidadTrabajo.UsuarioAplicacion.ObtenerPrimero(u => u.Id == id);
+
+            if (usuario == null)
+            {
+                return Json(new {success = false, message = "Error de usuario"});
+            }
+
+            if (usuario.LockoutEnd != null && usuario.LockoutEnd > DateTime.Now)
+            {
+                usuario.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                usuario.LockoutEnd = DateTime.Now.AddYears(1);
+            }
+
+            await unidadTrabajo.Guardar();
+            return Json(new { success = true, message = "Operacion Exitosa" });
+        }
+
+        #endregion
+    }
+}
---
+++ SistemaInventarioCore/wwwroot/js/usuario.js
@@ -0,0 +1,68 @@
+﻿
+var datatable;
+
+$(document).ready(function () {
+    CargarTabla();
+});
+
+const CargarTabla = () => {
+    datatable = $("#tblDato").DataTable({
+        "ajax": {
+            "url": "/Admin/Usuarios/ObtenerTodos"
+        },
+        "columns": [
+            { "data": "nombre" },
+            { "data": "apellido" },
+            { "data": "email" },
+            { "data": "phoneNumber" },
+            { "data": "role" },
+            {
+                "data": {
+                    id: "id", lockoutEnd: "lockoutEnd"
+                },
+                "render": function (data) {
+                    let hoy = new Date().getTime();
+                    let bloqueo = new Date(data.lockoutEnd).getTime();
+                    if (bloqueo > hoy) {
+                        // Usuario esta Bloqueado
+                        return `
+                            <div class="text-center">
+                               <a onclick=BloquearDesbloquear('${data.id}') class="btn btn-danger btn-sm text-white" style="cursor:pointer">
+                                    <i class="fa-solid fa-lock"></i>
+                               </a> 
+                            </div>
+                        `;
+                    }
+                    else {
+                        return `
+                            <div class="text-center">
+                               <a onclick=BloquearDesbloquear('${data.id}') class="btn btn-success btn-sm text-white" style="cursor:pointer" >
+                                    <i class="fa-solid fa-unlock"></i>
+                               </a> 
+                            </div>
+                        `;
+                    }
+
+                }
+            }
+        ]
+    });
+}
+
+const BloquearDesbloquear = id => {
+
+    $.ajax({
+        type: "POST",
+        url: '/Admin/Usuarios/BloquearDesbloquear',
+        data: JSON.stringify(id),
+        contentType: "application/json",
+        success: function (id) {
+            if (id) {
+                toastr.success(id.message);
+                datatable.ajax.reload();
+            } else {
+                toastr.error(id.message);
+            }
+        }
+    });
+}
